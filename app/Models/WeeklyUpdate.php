4ada25c2ce1ecf8ed0c55427ca96a88ae70463a4
Hasan-Raza-InterSys <hasanintersysltd@gmail.com>
2025-09-29 20:20:41 +0500

    Store using user id

--- app/Models/WeeklyUpdate.php
+++ app/Models/WeeklyUpdate.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use App\Models\User;
+use App\Models\Gurukal;
 class WeeklyUpdate extends Model
 {
     use SoftDeletes;
